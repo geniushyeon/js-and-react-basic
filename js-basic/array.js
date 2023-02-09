d68a9dfdbe145de5828bfb77e1a9de2972dd315f
geniushyeon <jhlee0912@icloud.com>
2023-02-09 23:53:27 +0900

    add: 배열 학습

---
+++ js-basic/array.js
@@ -0,0 +1,10 @@
+let array = new Array() // 생성자 방식
+let arr = [1, 2, 3, 4] // 배열 리터럴
+let arr2 = [1, "2", null, true, undefined] // 자료형이 달라도 넣을 수 있음
+console.log(arr)
+console.log(arr2)
+
+// 배열에 요소 추가
+arr.push(5)
+// 배열의 길이
+console.log(arr.length)
